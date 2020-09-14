5fd148982d69a1a34029751792facadeddc1180f
Kelly Ruan <kruan@bu.edu>
2020-09-13 21:58:07 -0400

    adding mall to city with height

--- 01-python/leisure/mall.py
+++ 01-python/leisure/mall.py
@@ -1,3 +1,22 @@
-def draw_mall():
-    print("mall not found")
+def draw_mall(height=14):
+    if (height < 13):
+        raise ValueError("Height must be at least 13.")
+
+    print("                 __________________                 ")
+    print("                |     |      |     |                ")
+    print("                |     |      |     |                ")
+    print("                |_____|______|_____|                ")
+    print(" _______________|                  |_______________ ")
+    print("|   _________   |      M A L L     |   _________   |")
+
+    for i in range(height-13):
+        print("|               |                  |               |")
+
+    print("|               |      ______      |               |")
+    print("|               |     |      |     |               |")
+    print("|   _________   |     |      |     |   _________   |")
+    print("|  |         |  |     |      |     |  |         |  |")
+    print("|  |         |  |     |      |     |  |         |  |")
+    print("|  |         |  |     |    o |     |  |         |  |")
+    print("|__|_________|__|_____|______|_____|__|_________|__|")
     return
