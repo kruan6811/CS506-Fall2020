3e040fb973f6ac819f28903937e78ed2951e21c2
Kelly Ruan <kruan@bu.edu>
2020-09-19 20:17:17 -0400

    implemented library and passed all tests

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,21 @@
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='"')
+
+        ret = []
+        for row in reader:
+            curr_row = []
+            for x in row[0].split(','):
+                if x.isnumeric():
+                    curr_row += [int(x)]
+                else:
+                    curr_row += [(x)]
+            ret += [curr_row]
+        
+        return ret
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,39 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    if (x == [] or y == []):
+        raise ValueError("lengths must not be zero")
+    if (len(x) != len(y)):
+        raise ValueError("lengths must be equal")
+
+    return sum([abs(x[i] - y[i])**2 for i in range(len(x))])**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    if (x == [] or y == []):
+        raise ValueError("lengths must not be zero")
+    if (len(x) != len(y)):
+        raise ValueError("lengths must be equal")
+    
+    return sum([abs(x[i] - y[i]) for i in range(len(x))])
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if (x == [] or y == []):
+        raise ValueError("lengths must not be zero")
+
+    return 1 - len(set(x) & set(y)) / len(set(x) | set(y))
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if (x == [] or y == []):
+        raise ValueError("lengths must not be zero")
+    if (len(x) != len(y)):
+        raise ValueError("lengths must be equal")
 
-# Feel free to add more
+    sumx = 0
+    sumy = 0
+    sumxy = 0
+    for i in range(len(x)):
+        x_i = x[i]
+        y_i = y[i]
+        sumx += x_i**2
+        sumy += y_i**2
+        sumxy += x_i*y_i
+    
+    return sumxy/(sumx*sumy)**(1/2)
